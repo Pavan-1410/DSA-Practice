47bf0cf3b3dc7341a6dc6c87dde45b37b80fe9e5
Anshul Singh <anshulsingh024@yahoo.com>
2024-10-21 00:08:08 +0530

    Largest Format Updated

--- LARGESTARRAY.cpp
+++ LARGESTARRAY.cpp
@@ -1,22 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main()
-{
-    // where n is size of array arr
-    int n, max = 0;
-    cin >> n;
-    int arr[100000];
-
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] > max)
-            max = arr[i];
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        // code here
+        int max = 0;
+        for (auto i:arr)
+        {
+            if (i > max)
+                max = i;
+        }
+        
+        return max;
     }
-    cout<<max<<endl;
-
-    return 0;
-}
+};
